9d6d9b07865c4f0539eb1727deaff154d5ea5f8d
Gaurav <gauravagerwala@gmail.com>
2016-02-08 00:29:50 +0530

    Added post request to add todo

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Finish this course',
-  completed: false
-},{
-  id: 2,
-  description: 'Do competitive',
-  completed: false
-},{
-  id: 3,
-  description: 'Finish soft assignment',
-  completed: true
-}];
+var todos = [];
+var todoNextid = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('ToDo API Root');
@@ -38,6 +31,16 @@ app.get('/todos/:id', function(req, res){
     res.status(404).send();
   }
 });
+
+app.post('/todos', function(req, res){
+  var body  = req.body;
+
+  body.id = todoNextid++;
+
+  todos.push(body);
+
+  res.send(todos)
+})
 
 app.listen(PORT, function(){
   console.log('Express listening on port ' + PORT);
