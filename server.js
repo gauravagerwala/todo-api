06e9257f4644d2cbbca4c52615222af40225a7bf
Gaurav <gauravagerwala@gmail.com>
2016-02-12 13:00:26 +0530

    added sequelize to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -111,6 +112,8 @@ app.put('/todos/:id', function(req, res){
 
 });
 
-app.listen(PORT, function(){
-  console.log('Express listening on port ' + PORT);
+db.sequelize.sync().then( function(){
+  app.listen(PORT, function(){
+    console.log('Express listening on port ' + PORT);
+  });
 });
