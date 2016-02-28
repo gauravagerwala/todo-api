8894382c42238ccae49596e87bd46828e072d7f8
Gaurav <gauravagerwala@gmail.com>
2016-02-28 21:23:16 +0530

    heroku debugging

--- db.js
+++ db.js
@@ -16,8 +16,9 @@ else{
 
 var db = {};
 
-db.todo = sequelize.import(__dirname + '/models/todo.js');
+
 db.user = sequelize.import(__dirname + '/models/user.js');
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
