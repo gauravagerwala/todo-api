fdf47ea36bfeccc1628d56b986e257b828a5547a
Gaurav <gauravagerwala@gmail.com>
2016-03-01 19:37:54 +0530

    Added authentication token to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var crypto = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function( sequelize, DataTypes){
 	var user = sequelize.define('user', {
@@ -68,6 +70,24 @@ module.exports = function( sequelize, DataTypes){
 			toPublicJSON: function(){
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
+			},
+			generateToken: function(type){
+				if(!_.isString(type)){
+					return undefined;
+				}
+
+				try{
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encrytedData = crypto.AES.encrypt(stringData, 'abcdefgh123!@#').toString();
+					var token = jwt.sign({
+						token: encrytedData
+					}, 'qwerty012');
+
+					return token;					
+
+				} catch(e){
+					return undefined;
+				}
 			}
 		}
 	});
